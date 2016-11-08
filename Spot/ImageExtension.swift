78e4b29cdf3a30b6b0e12e3c0e020bb1fa3d79e8
Mats Becker <mats.becker@gmail.com>
2016-11-08 17:35:33 +0100

    First commit

---
+++ Spot/ImageExtension.swift
@@ -0,0 +1,91 @@
+//
+//  ImageExtension.swift
+//  Spot
+//
+//  Created by Mats Becker on 11/6/16.
+//  Copyright © 2016 safari.digital. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    /// Returns a image that fills in newSize
+    func resizedImage(newSize: CGSize) -> UIImage {
+        // Guard newSize is different
+        guard self.size != newSize else { return self }
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0);
+        self.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+    
+    /// Returns a resized image that fits in rectSize, keeping it's aspect ratio
+    /// Note that the new image size is not rectSize, but within it.
+    func resizedImageWithinRect(rectSize: CGSize) -> UIImage {
+        let widthFactor = size.width / rectSize.width
+        let heightFactor = size.height / rectSize.height
+        
+        var resizeFactor = widthFactor
+        if size.height > size.width {
+            resizeFactor = heightFactor
+        }
+        
+        let newSize = CGSize(width: size.width / resizeFactor, height: size.height / resizeFactor)
+        let resized = resizedImage(newSize: newSize)
+        return textToImage(drawText: "\(newSize.width)x\(newSize.height)" as NSString, inImage: resized, atPoint: CGPoint(x: 20, y: 20))
+    }
+    
+}
+
+func textToImage(drawText: NSString, inImage: UIImage, atPoint: CGPoint) -> UIImage{
+    
+    // Setup the font specific variables
+    let font = UIFont.boldSystemFont(ofSize: 16)
+    
+    // Setup the image context using the passed image
+    UIGraphicsBeginImageContextWithOptions(inImage.size, false, 0.0)
+    
+    // Setup the font attributes that will be later used to dictate how the text should be drawn
+    
+    let attr = [NSFontAttributeName: font, NSForegroundColorAttributeName:UIColor.white]
+    
+    // Put the image into a rectangle as large as the original image
+    inImage.draw(in: CGRect(x: 0, y: 0, width: inImage.size.width, height: inImage.size.height))
+    
+    // Create a point within the space that is as bit as the image
+    let rect = CGRect(x: atPoint.x, y: atPoint.y, width: inImage.size.width, height: inImage.size.height)
+    
+    // Draw the text into an image
+    drawText.draw(in: rect, withAttributes: attr)
+    drawText.draw(at: CGPoint(x: 20, y: 20), withAttributes: attr)
+    
+    // Create a new image out of the images we have created
+    let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+    
+    // End the context now that we have the image we need
+    UIGraphicsEndImageContext()
+    
+    //Pass the image back up to the caller
+    return newImage
+    
+}
+
+extension UIImage {
+    class func colorForNavBar(color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 100, height: 5)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        
+        context!.setFillColor(color.cgColor)
+        context!.fill(rect)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image!
+    }
+}
+
---
+++ Spot/ParkASViewController.swift
@@ -0,0 +1,182 @@
+//
+//  ParkASViewController.swift
+//  Spot
+//
+//  Created by Mats Becker on 11/6/16.
+//  Copyright © 2016 safari.digital. All rights reserved.
+//
+
+import UIKit
+import AsyncDisplayKit
+import Firebase
+import FirebaseDatabase
+
+class ParkASViewController: ASViewController<ASDisplayNode> {
+    
+    /**
+     * Firebase
+     */
+    var ref: FIRDatabaseReference!
+    var refs: [FIRDatabaseReference] = [FIRDatabaseReference]()
+    
+    /**
+    * AsyncDisplayKit
+    */
+    var tableNode: ASTableNode {
+        return node as! ASTableNode
+    }
+    
+    /**
+     * Data
+     */
+    let tableData: [Dictionary<String, String>] = [
+        [
+            "name"  :   "Attractions",
+            "path"  :   "park/addo/attractions"
+        ]
+        ,
+        [
+            "name"  :   "Animals",
+            "path"  :   "park/addo/animals"
+        ]
+    ]
+    
+    init() {
+        super.init(node: ASTableNode(style: UITableViewStyle.grouped))
+        tableNode.delegate = self
+        tableNode.dataSource = self
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("storyboards are incompatible with truth and beauty")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableNode.view.tableHeaderView = tableHeaderView
+        
+        // Firebase
+        ref = FIRDatabase.database().reference()
+        ref.child("sections").observeSingleEvent(of: .value, with: { (snapshot) in
+            let snaps = snapshot.value as! [String : NSDictionary]
+            for snap in snaps {
+                print(snap)
+            }
+            // self.tableNode.view.reloadData()
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationItem.titleView = navHeaderView(park: "Addo Elephant Park")
+        
+        // TableView
+        self.tableNode.view.showsVerticalScrollIndicator = false
+        self.tableNode.backgroundColor = UIColor.white
+        self.tableNode.view.separatorColor = UIColor.clear
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    lazy var tableHeaderView: UIView = {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 1, height: 28))
+        view.backgroundColor = UIColor.red
+        return view
+    }()
+    
+    func navHeaderView(park: String) -> UIView {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: (self.navigationController?.navigationBar.frame.size.height)!))
+        let title = UILabel()
+        title.attributedText = NSAttributedString(
+            string: park,
+            attributes: [
+                NSFontAttributeName: UIFont(name: "AvenirNext-DemiBold", size: 16)!,
+                NSForegroundColorAttributeName: UIColor.black
+                ])
+        title.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(title)
+        title.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        title.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        return view
+    }
+    
+    func sectionHeaderView(text: String) -> UIView {
+        let view = UIView(frame: CGRect.zero)
+        view.backgroundColor = UIColor(red:0.84, green:0.84, blue:0.84, alpha:1.00)
+        
+        let title = UILabel()
+        title.attributedText = NSAttributedString(
+            string: text,
+            attributes: [
+                NSFontAttributeName: UIFont.boldSystemFont(ofSize: 21),
+                NSForegroundColorAttributeName: UIColor(red:0.24, green:0.24, blue:0.24, alpha:1.00), // Baltic sea
+                NSBackgroundColorAttributeName: UIColor.clear,
+                NSKernAttributeName: 0.0,
+                ])
+        title.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(title)
+        
+        let constraintLeftTitle = NSLayoutConstraint(item: title, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1, constant: 20)
+        let constraintCenterYTitle = NSLayoutConstraint(item: title, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: 0)
+        
+        view.addConstraint(constraintLeftTitle)
+        view.addConstraint(constraintCenterYTitle)
+        
+        return view
+    }
+
+}
+
+extension ParkASViewController : ASTableDataSource {
+    func tableNode(_ tableNode: ASTableNode, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in tableNode: ASTableNode) -> Int {
+        return self.tableData.count
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return sectionHeaderView(text: tableData[section]["name"]!)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 44
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 28
+    }
+    
+    func tableNode(_ tableNode: ASTableNode, nodeForRowAt indexPath: IndexPath) -> ASCellNode {
+        let node = ASTextCellNode()
+        node.text = String(format: "Name: %ld\nPath: %ld", tableData[indexPath.section]["name"]!, tableData[indexPath.section]["path"]!)
+        node.backgroundColor = UIColor(red:0.93, green:0.40, blue:0.44, alpha:1.00)
+        return node
+    }
+}
+
+extension ParkASViewController : ASTableDelegate {
+    
+    func tableNode(_ tableNode: ASTableNode, constrainedSizeForRowAt indexPath: IndexPath) -> ASSizeRange {
+        return ASSizeRange.init(min: CGSize(width: 0, height: 180), max: CGSize(width: 0, height: 180))
+    }
+    
+    
+    func tableNode(_ tableNode: ASTableNode, didSelectRowAt indexPath: IndexPath) {
+        print("Row selected at: \(indexPath)")
+    }
+}
