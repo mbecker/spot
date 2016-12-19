f7b356328914cc7c072eeb365697f9ef204ba20f
Mats Becker <mats.becker@gmail.com>
2016-12-19 11:55:08 +0100

    Update table select

---
+++ Spot/Detail/CountryASCellNode.swift
@@ -0,0 +1,13 @@
+//
+//  CountryASCellNode.swift
+//  Spot
+//
+//  Created by Mats Becker on 16/12/2016.
+//  Copyright © 2016 safari.digital. All rights reserved.
+//
+
+import UIKit
+
+class CountryASCellNode: ASCellNode {
+
+}
---
+++ Spot/Detail/SeparatorASCellNode.swift
@@ -0,0 +1,13 @@
+//
+//  SeparatorASCellNode.swift
+//  Spot
+//
+//  Created by Mats Becker on 16/12/2016.
+//  Copyright © 2016 safari.digital. All rights reserved.
+//
+
+import UIKit
+
+class SeparatorASCellNode: ASCellNode {
+
+}
